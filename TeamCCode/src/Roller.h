127ef0e183a3392787c134121674eb8da9137596
AnnetteVega <83847126+AnnetteVega@users.noreply.github.com>
2022-10-01 10:29:30 -0500

    J

--- TeamCCode/src/Roller.h
+++ TeamCCode/src/Roller.h
@@ -4,9 +4,15 @@ extern int ROLLER_PORT, ROLLER_VOLTAGE;
 class Roller{
     public: 
     Motor rollerMotor = Motor(ROLLER_PORT);
+    Roller(){
+        rollerMotor.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+    }
     void spin()
     {
         rollerMotor.move(ROLLER_VOLTAGE);
+    }
+    void stop(){
+        rollerMotor.move_velocity(0);
     }
 };
  
