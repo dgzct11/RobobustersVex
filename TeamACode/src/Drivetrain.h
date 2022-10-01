cbf4530f89e9e66d445c58b0a82a578ca386a927
Nelson Vo <54444005+nelsonvo1234@users.noreply.github.com>
2022-10-01 11:56:43 -0500

    Update Drivetrain.h

--- TeamACode/src/Drivetrain.h
+++ TeamACode/src/Drivetrain.h
@@ -1,42 +1,45 @@
 #include "main.h"
-#include "pros/motors.h"
 #include "pros/motors.hpp"
-
-extern int FRONT_LEFT_MOTOR_PORT;
-extern int FRONT_RIGHT_MOTOR_PORT;
-extern int BACK_LEFT_MOTOR_PORT;
-extern int BACK_RIGHT_MOTOR_PORT;
-
+extern int LEFT_BACK_PORT;
+extern int LEFT_FRONT_PORT;
+extern int RIGHT_BACK_PORT;
+extern int RIGHT_FRONT_PORT;
 using namespace pros;
-
-class Drivetrain{
+class DriveTrain{
     public:
-    Motor leftFront = Motor(FRONT_LEFT_MOTOR_PORT);
-    Motor rightFront = Motor(FRONT_RIGHT_MOTOR_PORT);
-    Motor leftBack = Motor(BACK_LEFT_MOTOR_PORT);
-    Motor rightBack = Motor(BACK_RIGHT_MOTOR_PORT);
-   
-    Motor_Group leftMotor = Motor_Group({leftFront, leftBack});;
-    Motor_Group rightMotor = Motor_Group({leftFront, rightFront});
-    Drivetrain(){
+    Motor leftBack = Motor( LEFT_BACK_PORT);
+    Motor leftFront = Motor(LEFT_FRONT_PORT);
+    Motor rightBack = Motor(RIGHT_FRONT_PORT);
+    Motor rightFront = Motor(RIGHT_BACK_PORT);
+    Motor_Group leftMotor = Motor_Group({leftFront, leftBack});
+    Motor_Group rightMotor = Motor_Group({rightFront, rightBack});
+
+    DriveTrain(){
         leftMotor.set_reversed(true);
-    }
+        leftMotor.set_brake_modes(E_MOTOR_BRAKE_HOLD);
+        rightMotor.set_brake_modes(E_MOTOR_BRAKE_HOLD);
 
-    void tankDrive(int leftY, int rightY)
-    {
-        leftMotor.move(leftY);
-        rightMotor.move(rightY);
     }
-
-    void arcadeDrive(int leftY, int rightX)
-    {
+    void tankDrive(int leftY, int rightY){
+        leftMotor.move( leftY);
+        rightMotor.move( rightY);
+        if(!overThreshhold(leftY))
+        {
+            leftMotor.move_velocity(0);
+        }
+        if(!overThreshhold(rightY))
+        {
+           rightMotor.move_velocity(0);
+        }
+    }
+    void arcadeDrive(int leftY, int rightX){
         leftMotor.move(leftY + rightX);
         rightMotor.move(leftY - rightX);
     }
-
-
-
-
-
-
+    
+    bool overThreshhold(int value)
+    {
+        
+        return (abs(value)>=1);
+    }
 };
