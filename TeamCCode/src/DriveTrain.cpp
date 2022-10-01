47c472b7bc5f385a4fc2d2c5582dbce5afd4329a
Gufty <114815951+Gufty@users.noreply.github.com>
2022-10-01 10:02:29 -0500

    a2

--- TeamCCode/src/DriveTrain.cpp
+++ TeamCCode/src/DriveTrain.cpp
@@ -1,15 +1,27 @@
 #include "main.h"
+#include "pros/motors.hpp"
+extern int LEFT_BACK_PORT;
+extern int LEFT_FRONT_PORT;
+extern int RIGHT_BACK_PORT;
+extern int RIGHT_FRONT_PORT;
 using namespace pros;
 class DriveTrain{
     public:
-    Motor leftBack;
-    Motor leftFront;
-    Motor rightBack;
-    Motor rightFront;
+    Motor leftBack = Motor( LEFT_BACK_PORT);
+    Motor leftFront = Motor(LEFT_FRONT_PORT);
+    Motor rightBack = Motor(RIGHT_FRONT_PORT);
+    Motor rightFront = Motor(RIGHT_BACK_PORT);
+    Motor_Group leftMotor = Motor_Group({leftFront, leftBack});
+    Motor_Group rightMotor = Motor_Group({rightFront, rightBack});
+
     DriveTrain(){
-        leftBack = Motor(extern LEFT_BACK_PORT);
-        leftFront = Motor(extern LEFT_FRONT_PORT);
-        rightFront = Motor(extern RIGHT_FRONT_PORT);
-        leftBack = Motor(extern RIGHT_BACK_PORT);
+        rightMotor.set_reversed(true);
     }
-}
+    void tankDrive(int leftY, int rightY){
+        leftMotor.move(voltage: leftY);
+        rightMotor.move(voltage: rightY);
+    }
+    void arcadeDrive(int leftx, int rightY){
+
+    }
+};
