54948551273120255e4e05f9177b2f8b5b7e9bdf
Aztro-dev <davidperezcastellanos@gmail.com>
2022-10-05 15:54:00 -0500

    Overloaded some goofy operators for Vector2

--- TeamACode/src/utils.cpp
+++ TeamACode/src/utils.cpp
@@ -8,4 +8,20 @@ enum Drive{
 struct Vector2{
 	double x;
 	double y;
+
+	Vector2 operator+(Vector2 otherVector){
+		return {x + otherVector.x, y + otherVector.y};
+	}
+
+	Vector2 operator-(Vector2 otherVector){
+		return {x - otherVector.x, y - otherVector.y};
+	}
+
+	Vector2 operator*(Vector2 otherVector){
+		return {x * otherVector.x, y * otherVector.y};
+	}
+
+	Vector2 operator/(Vector2 otherVector){
+		return {x / otherVector.x, y / otherVector.y};
+	}
 };
