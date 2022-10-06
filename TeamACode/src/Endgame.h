fa76d93bd0baa0e0f196235e77300be5ff78d0b3
Aztro-dev <davidperezcastellanos@gmail.com>
2022-10-06 09:47:21 -0500

    Added endgame mech that works with a piston

---
+++ TeamACode/src/Endgame.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+using namespace pros;
+
+#define PNEUMATICS_ADI_PORT 'A'
+
+class Endgame{
+	public:
+	bool endgameState = false;
+	ADIDigitalOut pneumatics = ADIDigitalOut(PNEUMATICS_ADI_PORT);
+
+	void Shoot(){
+		pneumatics.set_value(!endgameState);
+	}
+};
