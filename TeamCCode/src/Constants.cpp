a7acc02eea06011312918c4fdf2da26af0a945f1
Aztro-dev <davidperezcastellanos@gmail.com>
2022-10-06 10:16:26 -0500

    Removed whip code and made constants actually constant

--- TeamCCode/src/Constants.cpp
+++ TeamCCode/src/Constants.cpp
@@ -1,8 +1,6 @@
-    int LEFT_BACK_PORT=12;
-    int LEFT_FRONT_PORT=11;
-    int RIGHT_BACK_PORT=2;
-    int RIGHT_FRONT_PORT=1;
-    int ROLLER_PORT = 5;
-    int ROLLER_VOLTAGE = 75;
-    int WHIP_PORT = 7;
-    int WHIP_VOLTAGE = 75;
+const int LEFT_BACK_PORT=12;
+const int LEFT_FRONT_PORT=11;
+const int RIGHT_BACK_PORT=2;
+const int RIGHT_FRONT_PORT=1;
+const int ROLLER_PORT = 5;
+const int ROLLER_VOLTAGE = 75;
