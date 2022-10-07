c90c7548589aa56c15c87717950a873d76cd18cd
Aztro-dev <davidperezcastellanos@gmail.com>
2022-10-07 18:23:39 -0500

    Added robot.h file with DistanceTo() function

---
+++ TeamACode/src/Robot.h
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "Drivetrain.h"
+#include "Roller.h"
+
+class Robot{
+  public:
+    Vector2 pos;
+    Drivetrain drivetrain;
+    Roller roller;
+    Endgame endgame;
+
+    Robot(Drivetrain drivetrain, Roller roller, Endgame endgame) : drivetrain(drivetrain), roller(roller), endgame(endgame){
+      pos = drivetrain.pos;
+    }
+
+    double DistanceTo(Vector2 inpos){
+      double xDistance = fabs(pos.x - inpos.x);
+      double yDistance = fabs(pos.y - inpos.y);
+
+      return sqrt(pow(xDistance,2) + pow(yDistance, 2));
+    }
+
+};
+
