8a65dbf1614a936951334b65a18b5f8b128304b6
Gufty <114815951+Gufty@users.noreply.github.com>
2022-10-24 20:02:09 -0500

    Line class???

--- TeamCCode/src/LineDis.h
+++ TeamCCode/src/LineDis.h
@@ -1,6 +1,24 @@
 #pragma once
+#include <iostream>
+#include <cmath>
+#include "Utils.hpp"
 class Line{
     public:
-        int 
-        
+    double x1, y1, x2, y2, ratio, disBtwnCords, disToCordx, disToCordy;
+    Vector2 startPos;
+    Vector2 endPos;
+    Line(Vector2 startPos, Vector2 endPos){
+      this->startPos = startPos;
+      this->endPos = endPos;
+    }
+    Vector2 distanceToCoordinate(double dis){
+      Vector2 disToCord = endPos.operator-(startPos);
+      disToCordx = pow(disToCord.x, 2);
+      disToCordy = pow(disToCord.y, 2);
+      disBtwnCords = sqrt(disToCord.x + disToCord.y);
+      ratio = dis/disBtwnCords;
+      disToCord.x *= ratio;
+      disToCord.y *= ratio;
+      return disToCord;
+    }
 };
