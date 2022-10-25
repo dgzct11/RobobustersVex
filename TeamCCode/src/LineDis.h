0f60207cbd8138a935601a952ea499154b209d3d
Gufty <114815951+Gufty@users.noreply.github.com>
2022-10-25 11:43:07 -0500

    made DisToCord outside method

--- TeamCCode/src/LineDis.h
+++ TeamCCode/src/LineDis.h
@@ -7,12 +7,13 @@ class Line{
     double x1, y1, x2, y2, ratio, disBtwnCords, disToCordx, disToCordy;
     Vector2 startPos;
     Vector2 endPos;
+    Vector2 disToCord;
     Line(Vector2 startPos, Vector2 endPos){
       this->startPos = startPos;
       this->endPos = endPos;
     }
     Vector2 distanceToCoordinate(double dis){
-      Vector2 disToCord = endPos.operator-(startPos);
+      disToCord = endPos.operator-(startPos);
       disToCordx = pow(disToCord.x, 2);
       disToCordy = pow(disToCord.y, 2);
       disBtwnCords = sqrt(disToCordx + disToCordy);
